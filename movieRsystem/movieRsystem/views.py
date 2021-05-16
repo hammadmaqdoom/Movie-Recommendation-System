8f5c5eaba1fffb86421b6e42563055e296cd2238
hammadmaqdoom <hammadmaqdoom1@gmail.com>
2021-05-16 21:09:03 +0500

    Setup Django, URLS and Views

---
+++ movieRsystem/movieRsystem/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
